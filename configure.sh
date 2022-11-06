f61665c23aec8530a3583ccd774deaa2c6795b70
ksfuei <102844015+ksfuei@users.noreply.github.com>
2022-11-06 04:49:50 +0000

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# V2Ray new configuration
+# Run V2ray
+# Write V2Ray configuration
+cat << EOF > /etc/v2ray/config.json
+{
+    "inbounds": [{
+        "port": 8080,
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "65cc3403-fd0b-4450-b7c3-cfa7991adf41",
+                "alterId": 0
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "/lawrkrc"
+            }
+        }
+    }],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
+}
+EOF
+
+#Run V2ray
+
+/usr/bin/v2ray/v2ray  run -config=/etc/v2ray/config.json
